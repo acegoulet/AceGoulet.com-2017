ac8db775dd1ad8509d1f498b0bd8f3b1cdb6ad89
acegoulet <ace@acegoulet.com>
2017-10-12 14:26:50 -0700

    adding in social share links to single.php

--- templates/partials/entry-meta.php
+++ templates/partials/entry-meta.php
@@ -9,4 +9,19 @@
 			<span class="entry-meta-block"><strong>Updated: </strong><?php echo $mod_date; ?></span>
 		<?php } ?>
 	</p>
+	<?php if(is_single()){ ?>
+		<div class="share-links">
+			<p><strong>Share:</strong></p>
+			<?php 
+				$share_url = urlencode(get_permalink());
+				$share_title = urlencode(get_the_title());
+				$share_site_url = urlencode(get_site_url());
+			?>
+			<ul>
+				<li><a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $share_url; ?>" target="_blank">Facebook</a></li>
+				<li><a href="https://twitter.com/intent/tweet?url=<?php echo $share_url; ?>&text=<?php echo $share_title; ?>" target="_blank">Twitter</li>
+				<li><a href="https://www.linkedin.com/shareArticle?mini=true&url=<?php echo $share_url; ?>&source=<?php echo $share_site_url; ?>" target="_blank">LinkedIn</a></li>
+			</ul>
+		</div>
+	<?php } ?>
 </div>
